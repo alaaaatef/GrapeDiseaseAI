031e017ae28121749c071d331ac713c2012f941b
Nourseen Mahumed <nourseenmahumed@gmail.com>
2026-08-01 01:30:34 +0300

    Improve data augmentation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -14,23 +14,10 @@ import tensorflow as tf
 # ==================================================
 
 data_augmentation = tf.keras.Sequential([
-
-    tf.keras.layers.RandomFlip(
-        "horizontal"
-    ),
-
-    tf.keras.layers.RandomRotation(
-        0.10
-    ),
-
-    tf.keras.layers.RandomZoom(
-        0.10
-    ),
-
-    tf.keras.layers.RandomContrast(
-        0.10
-    )
-
+    tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+    tf.keras.layers.RandomRotation(0.15),
+    tf.keras.layers.RandomZoom(0.15),
+    tf.keras.layers.RandomContrast(0.1),
 ])
 
 # ==================================================
